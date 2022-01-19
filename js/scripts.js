d801eb4915de5db4a42b543c3c9dbfbe2163ee6d
Ultra Zacky <ultrazacky@gmail.com>
2022-01-19 14:24:23 -0800

    add first test to function. function works as intended

---
+++ js/scripts.js
@@ -0,0 +1,25 @@
+const vowels = ["a", "e", "i", "o", "u"];
+
+function vowelCounter(vowels,string) {
+  let vowelCount=0;
+  for (let i = 0; i < vowels.length; i+=1){
+      if (vowels[i] === string){
+        vowelCount ++;
+        console.log(vowelCount);
+        return vowelCount;
+      } else {
+        return 0;
+      }
+  }
+}
+
+
+/*
+let vowelCount = 0;
+for(let i = 0; i< vowels.length; i+=1){
+  if(vowels[i] === string){
+    vowelCount ++;
+    console.log(vowelCount);
+    return vowelCount;
+  }
+}*/
