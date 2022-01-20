7ca3fae86c4fd488846b21adfe5e53b0cf2b9331
Ultra Zacky <ultrazacky@gmail.com>
2022-01-19 16:47:23 -0800

    wordAy function --work-in-progress--

--- js/scripts.js
+++ js/scripts.js
@@ -19,3 +19,25 @@ function vowelTransformer(string){
     }
   }
 }
+
+function wordAy(string){
+  const vowels = ["a", "e", "i", "o", "u"];
+  let vowelIndex = 0;
+  for(let char of string){
+    if(vowelIndex.includes(char)){
+      vowelIndex = string.indexOf(char);
+      break;
+  }
+  return string.slice(vowels) + string.slice(0, vowels) + "ay";
+}
+
+
+/*function consonantTransformer(string){
+  const vowels = ["a","e","i","o","u"];
+  for(i = 0; i < vowels.length; i++){
+    if(string.charAt(0) != vowels[i]){
+      let string2 = string.substring(0);
+      console.log(string2 + string.charAt(0) + "ay");
+    }
+  }
+}*/
